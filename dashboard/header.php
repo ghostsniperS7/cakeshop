992d736f1e95ea8d0edfc56726659808c58ee99e
Safwan Asif <safwan2506e@aptechsite.net>
2026-08-17 06:51:26 -0700

    .

--- dashboard/header.php
+++ dashboard/header.php
@@ -322,7 +322,7 @@ session_start();
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" href="logout.php">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
